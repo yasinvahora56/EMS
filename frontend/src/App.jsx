c885a9bd84a11edad48e69f0bec874a2a49d1f41
yasinvahora56 <12iamyasin@gmail.com>
2025-02-19 13:02:40 +0530

    Added User Panel * Profile Section * Sidebar Panel ~ Added Folders User DashBoardLeaveRequest UserProfile LogOut Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,17 @@ import Profile from "./Components/Admin/Profile/Profile"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Attandance from "./Components/Admin/Attandance/Attandance"
 import SideNav from "./Components/Admin/Sidebar/SideNav"
+import SideBar from "./Components/User/Sidebar/SideBar"
 import Leave from "./Components/Admin/Leave/Leave"
 import Payroll from "./Components/Admin/Salary/Payroll"
+import UserProfile from "./Components/User/Profile/UserProfile"
 
 
 function App() {
 
   return (
     <Router>
-      <div className="flex">
+      {/* <div className="flex">
       <SideNav/>
       <div className="flex-1 p-4">
           <Routes>
@@ -23,6 +25,14 @@ function App() {
             <Route path="/leave" element={<Leave/>}/>
             <Route path="/payroll" element={<Payroll/>}/>
             <Route path="/profile" element={<Profile/>}/>
+          </Routes>
+        </div>
+      </div> */}
+      <div className="flex"> 
+      <SideBar/>
+        <div>
+          <Routes>
+            <Route path="/UserProfile" element={<UserProfile/>}></Route>
           </Routes>
         </div>
       </div>
---
+++ frontend/src/Components/User/Profile/UserProfile.jsx
@@ -0,0 +1,127 @@
+import { useState } from 'react';
+import logo from '../Images/logo.jpg';
+
+const UserProfile = () => {
+
+    const [modal, setModal] = useState();
+
+    const ToggleModal = () => {
+        setModal(!modal);
+    }
+
+    const Data = [
+        {
+            name: "Asad Master",
+            designation: "Devloper",
+            join_date: "01/02/2025",
+            email: "asadmaster@gmail.com",
+            phone: "7990980675",
+        },
+    ];
+    return (
+        <>
+        <div className='flex absolute top-30 left-180  max-h-screen'>
+            <div className='bg-white shadow-lg rounded-2xl p-6 max-w-sm text-center'>
+                <h1 className='text-3xl font-bold text-gray-800 mb-4 '>Admin Profile</h1>
+                {Data.map((admin, index) => (
+                    <div key={index} className='space-y-4'>
+                        <img src={logo} alt="Profile" className='w-24 h-24 mx-auto rounded-full border-4 border-gray-300' />
+                        <h2 className='text-xl font-semibold text-gray-900'>{admin.name}</h2>
+                        <p className='text-gray-600 '>{admin.designation}</p>
+                        <div className='text-left space-y-2 mt-4'>
+                            <div className='flex justify-between text-gray-700'>
+                                <span className='font-semibold'>Join Date:</span>
+                                <span>{admin.join_date}</span>
+                            </div>
+                            <div className='flex gap-10 justify-between text-gray-700'>
+                                <span className='font-semibold'>Email:</span>
+                                <span className=''>{admin.email}</span>
+                            </div>
+                            <div className='flex justify-between text-gray-700'>
+                                <span className='font-semibold'>Phone:</span>
+                                <span>{admin.phone}</span>
+                            </div>
+                        </div>
+                        <button 
+                        className='bg-blue-400 w-full px-4 py-2 rounded-full text-white cursor-pointer'
+                        onClick={ToggleModal}
+                        >Edit Profile</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+        
+        <div className=''>
+        {modal && (
+                <div className='fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center'>
+                    <div className='bg-white p-6 rounded-lg shadow-xl w-96'>
+                        {Data.map((editEmployee) => (
+                            <div key={editEmployee.email} className='space-y-4'>
+                                <div>
+                                    <label htmlFor="name">Name:</label>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        value={editEmployee.name}
+                                        className="w-full px-4 py-2 border rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="designation">Designation:</label>
+                                    <input
+                                        type="text"
+                                        name="designation"
+                                        value={editEmployee.designation}
+                                        className="w-full px-4 py-2 border rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="email">Email:</label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        value={editEmployee.email}
+                                        className="w-full px-4 py-2 border rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">Phone:</label>
+                                    <input
+                                        type="text"
+                                        name="phone"
+                                        value={editEmployee.phone}
+                                        className="w-full px-4 py-2 border rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">Uploade Image:</label>
+                                    <input
+                                        type="file" accept="image/*"
+                                        name="phone"
+                                        value={editEmployee.phone}
+                                        className="w-full px-4 py-2 border rounded-lg"
+                                    />
+                                </div>
+                                <div className="flex justify-between mt-4">
+                                    <button
+                                        className="bg-gray-300 px-4 py-2 rounded-full cursor-pointer"
+                                        onClick={ToggleModal}
+                                    >
+                                        Cancel
+                                    </button>
+                                    <button className='bg-blue-400 px-4 py-2 rounded-full text-white cursor-pointer'>
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+
+        </>
+    );
+};
+
+export default UserProfile;
