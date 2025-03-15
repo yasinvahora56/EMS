fa06341bdbaf40d8f3b1ec48b3ac64960c769199
yasinvahora56 <12iamyasin@gmail.com>
2025-03-15 12:04:50 +0530

    Date: 15/03/2025 Commit-time: 12:05 PM From: Office
    - Server Side login controller banavyu ane api fetch kari
    ane login seccess karyu
    - json web token import karu env ma JWT_CONNECTION key naakhi
    
    On branch Date-15-03-2025
    Changes to be committed:
    	modified:   Server/.env
    	modified:   Server/package-lock.json
    	modified:   Server/package.json
    	modified:   Server/src/Controller/Logincontroller.js
    	modified:   Server/src/Routes/Authrouter.js
    	modified:   Server/src/middlewares/LoginValidation.js
    	modified:   frontend/package-lock.json
    	modified:   frontend/package.json

--- Server/src/Routes/Authrouter.js
+++ Server/src/Routes/Authrouter.js
@@ -6,7 +6,7 @@ import  LoginValidation  from "../middlewares/LoginValidation.js"
 import express from "express"
 const router = express.Router()
 
-router.post('/signup', SignupValidation, signup)
 router.post('/login',LoginValidation, login)
+router.post('/signup', SignupValidation, signup)
 
 export default router
--- Server/src/middlewares/LoginValidation.js
+++ Server/src/middlewares/LoginValidation.js
@@ -3,12 +3,16 @@ import joi from "joi"
 const LoginValidation = (req, res, next) => {
     const schema = joi.object({
         employeeId:joi.string().min(2).max(10).required(),
-        Password: joi.string().min(6).max(10).required()
+        password: joi.string().min(6).max(10).required()
     })
     const {error} = schema.validate(req.body)
     if(error){
+        console.error("login Error", error)
         return res.status(400)
-            .json({message: "Bad Request"})
+            .json({
+                message: "Bad Request",
+                error
+            })
     }
     next()
 }
